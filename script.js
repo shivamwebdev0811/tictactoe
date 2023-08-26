5cb513c5cb2b4b189f89bf8897570ac04ff723c1
shivamwebdev0811 <shivamk.sk47994@gmail.com>
2023-08-26 10:36:04 +0530

    new commit added

---
+++ script.js
@@ -0,0 +1,116 @@
+const boxes= document.querySelectorAll('.box');
+const gameInfo=document.querySelector('.game-info');
+const newGameBtn=document.querySelector('.btn');
+
+let currentPlayer;
+let gameGrid;
+
+const winningPosition=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+// let's create a function to initialise the game
+function initGame()
+{
+
+    currentPlayer="X";
+    gameGrid=["","","","","","","","",""];
+    boxes.forEach((box,index)=>{
+        box.innerHTML="";
+        box.style.pointerEvents="all";
+        box.classList.remove("win");
+    })
+
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText= `Current player - ${currentPlayer}`;
+    // box.classList.remove="win";
+
+}
+initGame();
+
+boxes.forEach((box,index)=>
+{
+    box.addEventListener('click',()=>
+    {
+        handleClick(index);
+    });
+});
+
+function handleClick(index)
+{
+    if(gameGrid[index]==="")
+    {
+        boxes[index].innerHTML=currentPlayer;
+        gameGrid[index]=currentPlayer;
+        boxes[index].style.pointerEvents="none";
+        // turn change
+        swapTurn();
+        checkGameOver();
+    }
+}
+function checkGameOver(){
+    let answer="";
+    winningPosition.forEach((position)=>{
+        if((gameGrid[position[0]] !== "" && gameGrid[position[1]] !== "" && gameGrid[position[2]] !== "") && (gameGrid[position[0]]=== gameGrid[position[1]]) && (gameGrid[position[1]]===gameGrid[position[2]]))
+        {   
+            // check if winner is X
+            if(gameGrid[position[0]]==="X")
+            {
+                answer="X";
+            }
+            else 
+            {
+                answer="O";
+            }
+            // disable pointer event
+            boxes.forEach((box)=>{
+                box.style.pointerEvents="none";
+            })
+            boxes[position[0]].classList.add("win");
+            boxes[position[1]].classList.add("win");
+            boxes[position[2]].classList.add("win");
+            return;
+        }
+
+    });
+    if(answer!=="")
+    {
+        gameInfo.innerText=`Winner Player -${answer}`;
+        newGameBtn.classList.add("active"); 
+        
+    }
+    let fillCount=0;
+    gameGrid.forEach((box)=>
+    {
+        if(box!=="")
+        {
+            fillCount++;
+        }
+        
+    });
+    if(fillCount===9)
+    {
+        gameInfo.innerText="Game Tied !";
+        newGameBtn.classList.add("active");
+    }
+}
+function swapTurn()
+{
+    if(currentPlayer==="X")
+    {
+        currentPlayer="O";
+    }
+    else
+    {
+        currentPlayer="X";
+    }
+    gameInfo.innerText=`Current Player -${currentPlayer}`;
+}
+
+newGameBtn.addEventListener('click',initGame);
